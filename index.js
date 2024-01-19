a92d906e823cde2caf8a7d213ac7358cb75a0a7b
lyrium-haze <83539659+lyrium-haze@users.noreply.github.com>
2024-01-19 22:58:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,37 +4,40 @@ const { message } = require('telegraf/filters');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
-let movieArr = [];
+let smena = [];
 
 let now = new Date();
 now = now.toISOString();
 let slicer = now.indexOf('T');
 now = now.slice(0, 10);
 let i = 0;
+function kinokassaHandler(response, movieArr) {
+  while(i< response.data.releases.length){
+    let seances = []
+    response.data.releases[i].seances.forEach(function (seance){
+      let time = seance.start_date_time;
+      let slicer = time.indexOf('T') + 1;
+      time = time.slice(slicer);
+      slicer = time.indexOf('.');
+      time = time.slice(0, slicer);
+      time = time.slice(0, -3);
+      seances.push(time)
+    })
+      movieArr.push({ seances: seances, title: response.data.releases[i].title, poster: response.data.releases[i].poster,
+         genre : response.data.releases[i].genres[0].title });
+      i++;
+  }
+}
 axios({
     method: 'get',
     url: `https://kinokassa.kinoplan24.ru/api/v2/release/playbill?city_id=46&date=${now}`,
     headers: {'X-Application-Token': 'Pshm8LrxVQe19ouONPLD5SXWtaDhIKfM', 'X-Platform': 'widget'}
   })
     .then(function (response) {
-        while(i< response.data.releases.length){
-          let seances = []
-          response.data.releases[i].seances.forEach(function (seance){
-            let time = seance.start_date_time;
-            let slicer = time.indexOf('T') + 1;
-            time = time.slice(slicer);
-            slicer = time.indexOf('.');
-            time = time.slice(0, slicer);
-            time = time.slice(0, -3);
-            seances.push(time)
-          })
-            movieArr.push({ seances: seances, title: response.data.releases[i].title, poster: response.data.releases[i].poster,
-               genre : response.data.releases[i].genres[0].title });
-            i++;
-        }
+      kinokassaHandler(response, smena);
     });
 
-    let movieArr2 = [];
+    let mirage = [];
     let seances2 = [];
 
     axios({
@@ -57,33 +60,29 @@ axios({
           seances.forEach(function (seance) {
             seances2.push(seance.textContent);
           })
-          movieArr2.push({title: movie.textContent, seances: seances2, poster: poster, genre: genre});
+          mirage.push({title: movie.textContent, seances: seances2, poster: poster, genre: genre});
         })
       });
 
-      let movieArr3 = [];
+      let silver = [];
       axios({
         method: 'get',
         url: `https://kinokassa.kinoplan24.ru/api/v2/release/playbill?city_id=46&date=${now}`,
         headers: {'X-Application-Token': 'TpoJflUI4lY945APjVbo7nAHsLAhbw4m', 'X-Platform': 'widget'}
       })
         .then(function (response) {
-          while(i< response.data.releases.length){
-            let seances = []
-            response.data.releases[i].seances.forEach(function (seance){
-              let time = seance.start_date_time;
-              let slicer = time.indexOf('T') + 1;
-              time = time.slice(slicer);
-              slicer = time.indexOf('.');
-              time = time.slice(0, slicer);
-              time = time.slice(0, -3);
-              seances.push(time)
-            })
-              movieArr3.push({ seances: seances, title: response.data.releases[i].title, poster: response.data.releases[i].poster,
-                 genre : response.data.releases[i].genres[0].title});
-              i++;
-          }
+          kinokassaHandler(response, silver);
         })
+        let sovr = [];
+        axios({
+          method: 'get',
+          url: `https://kinokassa.kinoplan24.ru/api/v2/release/playbill?city_id=46&date=${now}`,
+          headers: {'X-Application-Token': '9kC5cjMhH2AWQiiSLswBI7RJrcepOwNz', 'X-Platform': 'widget'}
+        })
+          .then(function (response) {
+              kinokassaHandler(response, sovr);
+          });
+
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
@@ -91,20 +90,23 @@ let { Markup } = require("telegraf");
 
 function getMenu() {
   return Markup.keyboard([
-    ["Смена"],["Мираж"],["Сильвер Синема"]
+    ["Смена"],["Мираж"],["Сильвер Синема"],["Современник"]
   ]).resize();
 }
 bot.start(ctx => {
     ctx.reply('Здравствуйте! Выберите кинотеатр, чтобы узнать доступные сеансы:', getMenu())
 })
 bot.hears("Смена", (ctx) => {
-    movieArr.forEach((movie) => {ctx.replyWithHTML(`<b>${movie.title}</b>\n\n Жанр: ${movie.genre}\n\n` + `${movie.seances}` + `\n\n${movie.poster}`);})
+    smena.forEach((movie) => {ctx.replyWithHTML(`<b>${movie.title}</b>\n\n Жанр: ${movie.genre}\n\n` + `${movie.seances}` + `\n\n${movie.poster}`);})
 })
 bot.hears("Мираж", (ctx) => {
-  movieArr2.forEach((movie) => {ctx.replyWithHTML(`<b>${movie.title}</b>\n\n Жанр: ${movie.genre}\n\n` + `${movie.seances}`  + `\n\n${movie.poster}`)})
+  mirage.forEach((movie) => {ctx.replyWithHTML(`<b>${movie.title}</b>\n\n Жанр: ${movie.genre}\n\n` + `${movie.seances}`  + `\n\n${movie.poster}`)})
 })
 bot.hears("Сильвер Синема", (ctx) => {
-  movieArr3.forEach((movie) => {ctx.replyWithHTML(`<b>${movie.title}</b>\n\n Жанр: ${movie.genre}\n\n` + `${movie.seances}` + `\n\n${movie.poster}`)})
+  silver.forEach((movie) => {ctx.replyWithHTML(`<b>${movie.title}</b>\n\n Жанр: ${movie.genre}\n\n` + `${movie.seances}` + `\n\n${movie.poster}`)})
+})
+bot.hears("Современник", (ctx) => {
+  sovr.forEach((movie) => {ctx.replyWithHTML(`<b>${movie.title}</b>\n\n Жанр: ${movie.genre}\n\n` + `${movie.seances}` + `\n\n${movie.poster}`)})
 })
 
 bot.launch();
